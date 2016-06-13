2d4fc3827db19ae372eb43b2c7f8096820bbd4a4
Tanya Litvinenko <tanyalitvinenko976@gmail.com>
2016-06-13 04:30:35 +0300

    Update settings.py

--- conf_proj/settings.py
+++ conf_proj/settings.py
@@ -141,8 +141,8 @@ ACCOUNT_ACTIVATION_DAYS = 7
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'frrresshh@gmail.com'
-EMAIL_HOST_PASSWORD = 'nicola13'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
